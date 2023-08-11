7cf487ebfcca48ffa7c208dbfa77a072d8eec488
Waldir Junior <waldirjunior.gpn@gmail.com>
2023-08-11 13:21:05 -0300

    primeiro commit

---
+++ src/main/java/br/com/calculadoraDeFrete/CalculadoraDeFrete/CalculadoraDeFreteApplication.java
@@ -0,0 +1,13 @@
+package br.com.calculadoraDeFrete.CalculadoraDeFrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraDeFreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraDeFreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/calculadoraDeFrete/CalculadoraDeFrete/CalculadoraDeFreteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.calculadoraDeFrete.CalculadoraDeFrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraDeFreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
